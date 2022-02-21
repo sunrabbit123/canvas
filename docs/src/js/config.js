4ba00405cf60ffd454e2416e5f12e6a6fd39a3c8
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2022-02-21 20:28:18 +0900

    Update config.js

--- docs/src/js/config.js
+++ docs/src/js/config.js
@@ -1,4 +1,4 @@
 export const TREE_LINE_COLOR = '#000000';
-export const TREE_DEPTH = 20;
-export const LINE_WIDTH = 20;
-export const TREE_LENGTH = 100;
+export const TREE_DEPTH = 15;
+export const LINE_WIDTH = 10;
+export const TREE_LENGTH = 10;
