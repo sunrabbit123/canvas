52648559dee1cf0d1db636aeb0757e87cb0bc80b
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2022-02-21 19:22:45 +0900

    Update branch.js

--- docs/src/js/branch.js
+++ docs/src/js/branch.js
@@ -1,10 +1,25 @@
+import { TREE_LINE_COLOR, LINE_WIDTH } from './config.js';
+
 export class Branch{
   constructor(startX, startY, endX, endY){
     this.startX = startX;
     this.startY = startY;
     this.endX = endX;
     this.endY = endY;
+    this.color = TREE_LINE_COLOR;
+    this.lineWidth = LINE_WIDTH;
   }
   draw(ctx) {
+    ctx.beginPath();
+    
+    ctx.moveTo(this.startX, this.startY);
+    ctx.lineTo(this.endX, this.endY);
+    
+    ctx.lineWidth = this.lineWidth;
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = this.color;
+    
+    ctx.stroke();
+    ctx.closePath();
   }
 }
