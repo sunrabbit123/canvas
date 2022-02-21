4add51aa59176d7b7205d995d5ec9c81acfaf729
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2022-02-21 19:41:53 +0900

    Update branch.js

--- docs/src/js/branch.js
+++ docs/src/js/branch.js
@@ -1,13 +1,13 @@
 import { TREE_LINE_COLOR, LINE_WIDTH } from './config.js';
 
 export class Branch{
-  constructor(startX, startY, endX, endY){
+  constructor(startX, startY, endX, endY, lineWidth){
     this.startX = startX;
     this.startY = startY;
     this.endX = endX;
     this.endY = endY;
     this.color = TREE_LINE_COLOR;
-    this.lineWidth = LINE_WIDTH;
+    this.lineWidth = lineWidth;
   }
   draw(ctx) {
     ctx.beginPath();
@@ -15,7 +15,16 @@ export class Branch{
     ctx.moveTo(this.startX, this.startY);
     ctx.lineTo(this.endX, this.endY);
     
-    ctx.lineWidth = this.lineWidth;
+    if (this.lineWidth < 3) {
+      ctx.lineWidth = 0.5;
+    } else if (this.lineWidth < 7) {
+      ctx.lineWidth = this.lineWidth * 0.7;
+    } else if (this.lineWidth < 10) {
+      ctx.lineWidth = this.lineWidth * 0.9;
+    } else {
+      ctx.lineWidth = this.lineWidth;
+    }
+    
     ctx.fillStyle = this.color;
     ctx.strokeStyle = this.color;
     
