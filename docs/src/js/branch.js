bcd20ec349ae1e3b803850e6016e7061d48a4e5f
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2022-02-21 19:10:43 +0900

    Create branch.js

---
+++ docs/src/js/branch.js
@@ -0,0 +1,10 @@
+export class Branch{
+  constructor(startX, startY, endX, endY){
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+  }
+  draw(ctx) {
+  }
+}
