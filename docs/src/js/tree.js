255012d41d73eced2e5c9547bdcb580b9fb02d2c
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2022-02-21 19:39:24 +0900

    Update tree.js

--- docs/src/js/tree.js
+++ docs/src/js/tree.js
@@ -1,5 +1,5 @@
 import { Branch } from './branch.js';
-import { TREE_LENGTH } from './config.js';
+import { TREE_LENGTH, TREE_DEPTH } from './config.js';
 
 export class Tree {
   constructor(ctx, posX, posY) {
@@ -7,17 +7,28 @@ export class Tree {
     this.posX = posX;
     this.posY = posY;
     this.branches = []; // 가지들을 담을 공간
-
+    this.depth = TREE_DEPTH;
     this.init();
   }
 
   init() {
-    this.createBranch(this.posX, this.posY);
+    this.createBranch(this.posX, this.posY, -90, 0);
     this.draw(this.ctx);
   }
 
-  createBranch(startX, startY) {
-    this.branches.push(new Branch(startX, startY, startX, startY - TREE_LENGTH)); 
+  createBranch(startX, startY, angle, depth) {
+    if (depth === this.depth) return;
+    
+    const len = depth === 0 ? this.random(10, 13) : this.random(0, 11);
+    const depthConverse = this.depth - depth;
+    
+    const endX = startX + this.cos(angle) * len * depthConverse;
+    const endY = startY + this.sin(angle) * len * depthConverse;
+    const nextDepth = depth + 1;
+    this.branches.push(new Branch(startX, startY, endX, endY, depthConverse));
+    
+    this.createBranch(endX, endY, angle - this.random(15, 23), nextDepth);
+    this.createBranch(endX, endY, angle + this.random(15, 23), nextDepth);
   }
 
   draw(ctx) {
@@ -25,4 +36,9 @@ export class Tree {
       branch.draw(ctx);
     });
   }
+  
+  cos(angle){ return Math.cos(this.degToRad(angle)); }
+  sin(angle){ return Math.sin(this.degToRad(angle)); }
+  degToRad(angle){ return (angle / 180.0) * Math.PI; }
+  random(min, max) { return min + Math.floor(Math.random() * (max - min + 1)); }
 }
