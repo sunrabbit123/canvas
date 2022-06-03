b9d7bf2d6499d32b6c4660931bcaba2d18fb51f9
sunrabbit123 <qudwls185@naver.com>
2022-06-03 22:15:05 +0900

    add : move blocks

--- docs/tetris/js/board.js
+++ docs/tetris/js/board.js
@@ -12,6 +12,24 @@ class Board {
   getEmptyBoard() {
     return Array.from({ length: this.ROWS }, () => Array(this.COLS).fill(0));
   }
+
+  valid(p) {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return (
+          value === 0 || (this.isInsideWalls(x, y) && this.notOccupied(x, y))
+        );
+      });
+    });
+  }
+  isInsideWalls(x, y) {
+    return x >= 0 && x < this.COLS && y <= this.ROWS;
+  }
+  notOccupied(x, y) {
+    return this.grid[y] && this.grid[y][x] === 0;
+  }
 }
 
 export { Board };
--- docs/tetris/js/main.js
+++ docs/tetris/js/main.js
@@ -1,4 +1,4 @@
-import { COLS, BLOCK_SIZE, ROWS } from "./constants.js";
+import { COLS, BLOCK_SIZE, ROWS, KEY } from "./constants.js";
 import { Board } from "./board.js";
 import { Piece } from "./piece.js";
 
@@ -10,16 +10,42 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
+export const moves = {
+  [KEY.LEFT]: (p) => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
+  [KEY.UP]: (p) => ({ ...p, y: p.y + 1 }),
+};
+
 let board = new Board(COLS, ROWS);
 
-const playButton = document.getElementById("play");
-playButton.addEventListener("click", play);
-export function play() {
+function play() {
   board.reset();
-  let playBoard = board.getEmptyBoard();
 
   let piece = new Piece(ctx);
   piece.draw();
 
-  playBoard.piece = piece;
+  board.piece = piece;
 }
+
+document.getElementById("play").addEventListener("click", play);
+document.addEventListener("keydown", (event) => {
+  const { keyCode } = event;
+
+  if (moves[keyCode]) {
+    // 이벤트 버블링을 막는다.
+    event.preventDefault();
+
+    // 조각의 새 상태를 얻는다.
+    let p = moves[keyCode](board.piece);
+
+    if (board.valid(p)) {
+      // 이동이 가능한 상태라면 조각을 이동한다.
+      board.piece.move(p);
+
+      // 그리기 전에 이전 좌표를 지운다.
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+      board.piece.draw();
+    }
+  }
+});
