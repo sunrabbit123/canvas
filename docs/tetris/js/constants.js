afab91973132260340bfea7c409790cde8d3963f
sunrabbit123 <qudwls185@naver.com>
2022-06-04 17:03:09 +0900

    update: other keys

--- docs/tetris/js/constants.js
+++ docs/tetris/js/constants.js
@@ -3,8 +3,13 @@ export const ROWS = 20;
 export const BLOCK_SIZE = 30;
 
 export const KEY = {
+  ESC: 27,
+  SPACE: 32,
   LEFT: 37,
+  UP: 38,
   RIGHT: 39,
   DOWN: 40,
+  P: 80,
+  Q: 81,
 };
 Object.freeze(KEY);
